c9e30dbcdff652dc44cd45e878a14ad8fbcb0eae
Jabran Rafique <hello@jabran.me>
2014-01-07 19:50:06 +0000

    fix callback function

--- example/index.php
+++ example/index.php
@@ -1,31 +1,13 @@
 <?php 
-
 /**
- *
  * JavaScript library for Social media actions.
- *
- * @version: 1.3
+ * @version: 1.4
  * @author: hello@jabran.me
  * @link: http://github.com/jabranr / http://jabran.me
  * @package: SocialMediaDotjs
- * 
- * @license: MIT License
- * 
- * Copyright (c) 2013 Jabran Rafique
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
- * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
- * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
- * is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be 
- * included in all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
- $version = '1.3';
-
+$version = '1.4';
 ?>
 <!doctype HTML>
 <html>
@@ -60,16 +42,16 @@
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		})();
 
+		function testFunc(response)	{
+			console.log(response);
+		}
+
 		facebook.init({
 			appid:'142530249248955',
 			channelurl:'//jabran.me/channel.php',
 			status: true,
 			flrequests: true
-		}, function(FB)	{
-			FB.getLoginStatus(function(response)	{
-				console.log(response);
-			});
-		});
+		}, testFunc);
 	</script>
 	
 </head>
