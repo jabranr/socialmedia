f08fa29153d5bfe4f0d51982d7475d4e7a2fb8f1
Jabran Rafique <hello@jabran.me>
2014-04-12 09:00:26 +0100

    initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt)	{
+	grunt.initConfig({
+		
+		pkg: grunt.file.readJSON('package.json'),
+		
+		concat: {
+			dist: {
+				src: [
+					'dev/socialmedia.js',
+					'dev/*.js'
+				],
+				dest: 'src/socialmedia.js'
+			}
+		},
+
+		uglify: {
+			build: {
+				src: 'src/socialmedia.js',
+				dest: 'src/socialmedia.min.js'
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['dev/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+};
